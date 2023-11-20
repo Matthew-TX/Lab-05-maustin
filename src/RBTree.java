70b32df6c97d08567cb602c2e845e486d84fae6c
Matthew Austin <matthew.austintx@outlook.com>
2023-11-19 18:44:46 -0600

    Cleanup

--- src/RBTree.java
+++ src/RBTree.java
@@ -1,26 +1,7 @@
 public class RBTree {
     Node nil = null;
     Node root = null;
-    /*
-    RB-INSERT.T; ´/
-1 y D T:nil
-2 x D T:root
-3 while x ¤ T:nil
-4 y D x
-5 if z:key < x:key
-6 x D x:left
-7 else x D x:right
-8 ´:p D y
-9 if y = = T:nil
-10 T:root D ´
-11 elseif ´:key < y:key
-12 y:left D ´
-13 else y:right D ´
-14 ´:left D T:nil
-15 ´:right D T:nil
-16 ´:color D RED
-17 RB-INSERT-FIXUP.T; ´/
-     */
+
     public void Insert(Integer zKey){
         Node z = new Node();
         z.setKey(zKey);
